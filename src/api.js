2668dd1f2102dff39b3a0544392955f61b2854d4
Nityasha17 <nityashausgr1710@gmail.com>
2025-04-12 15:29:06 +0530

    Weather app with API Integration

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': process.env.REACT_APP_GEO_API_KEY,
+		'x-rapidapi-host': process.env.REACT_APP_GEO_API_HOST,
+	},
+};
+
+export const Geo_api_url = process.env.REACT_APP_GEO_API_URL;
+
+export const WEATHER_API_URL = process.env.REACT_APP_WEATHER_API_URL;
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
